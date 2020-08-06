abfe6ff3d2f5a600e21fbf60a1ecb7bf2007e13b
Jacob Fromm <jacobfromm@gmail.com>
2020-08-06 20:10:18 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,8 +4,7 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
   collection.each do |item|
-    return item if item[:item] == name
-    else return NIL
+    
   end   
 end
 
