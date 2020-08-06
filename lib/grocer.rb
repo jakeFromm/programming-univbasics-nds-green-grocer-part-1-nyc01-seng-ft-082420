96faea87a22b82691a3ae0171f4be5c1bfb649e3
Jacob Fromm <jacobfromm@gmail.com>
2020-08-06 20:50:04 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -15,7 +15,7 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   names = []
-  count = {}
+  count = Hash.new(0)
   cart.each do |item|
     names << item[:item]
   end
